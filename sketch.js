7359e545ddccd01d98709f92a7a42926a17cbd7b
Vedant Panchariya <vedantkp1510@gmail.com>
2025-08-25 15:59:46 +0530

    Now user can have boxes as per their choice
    Removed the random numbers in for loop and instead added the value of user entered and divided it by width and it worked

--- sketch.js
+++ sketch.js
@@ -1,41 +1,41 @@
-const container = document.getElementById('container');
-const input = document.getElementById('Enter');
-let number = 16;
-input.addEventListener('click',()=>{
+    const container = document.getElementById('container');
+    const input = document.getElementById('Enter');
+    let size = document.getElementById('number');
 
-    number = document.getElementById('number').value;
-});
-function color(){
-    const R = Math.floor(Math.random() * 256)
-    const G = Math.floor(Math.random() * 256)
-    const B = Math.floor(Math.random() * 256)
-    const color = `rgb(${R}, ${G}, ${B})`  
-    return color;
-}
-container.addEventListener('dblclick',()=>{
-    for(let j=0;j<21;j++){
-        for(let i=0;i<15;i++){
-            const box = document.createElement('div');
-            
-            box.style.width = number+'px';
-            box.style.height = number+'px'; 
-            box.style.border = '1px solid black'
-            
-            box.style.display = 'inline-block';
-            container.appendChild(box); 
-            box.addEventListener('mouseover',()=>{
-
-                box.style.backgroundColor = color();
-                
-            });
-            
-            container.appendChild(box); 
-        }
+    function color(){
+        const R = Math.floor(Math.random() * 256)
+        const G = Math.floor(Math.random() * 256)
+        const B = Math.floor(Math.random() * 256)
+        const color = `rgb(${R}, ${G}, ${B})`  
+        return color;
     }
-});
+    container.addEventListener('dblclick',()=>{
+        const val = parseInt(size.value);
 
-input.addEventListener('click',()=>{
-    container.innerHTML = '';
-    number = document.getElementById('number').value;
+        const width = container.clientWidth;
+        const boxsize = width / val;
+        container.style.alignContent = 'flex-start';
+        
 
-});
+        container.style.display = 'flex';
+        container.style.flexWrap = 'wrap'
+        for(let j=0;j<val;j++){
+            for(let i=0;i<val;i++){
+                const box = document.createElement('div');
+                
+                box.style.width = boxsize+'px';
+                box.style.height = boxsize+'px'; 
+                
+                box.addEventListener('mouseover',()=>{
+                    box.style.backgroundColor = color();
+                    
+                });
+                
+                container.appendChild(box); 
+            }
+        }
+    });
+    input.addEventListener('click',()=>{
+        container.innerHTML = '';
+        number = document.getElementById('number').value;
+    });
